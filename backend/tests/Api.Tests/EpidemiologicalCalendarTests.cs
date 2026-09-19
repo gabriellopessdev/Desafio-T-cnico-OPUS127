b5a80e4aed0be273c5a79ebb96df578129d31300
Gabriel Lopes <gabriellopessdev@gmail.com>
2026-09-19 17:44:42 -0300

    test: cobrir LastWeeks fechadas, fuso SP e ano epidemiologico

--- backend/tests/Api.Tests/EpidemiologicalCalendarTests.cs
+++ backend/tests/Api.Tests/EpidemiologicalCalendarTests.cs
@@ -41,4 +41,39 @@ public class EpidemiologicalCalendarTests
         ranges[1].EwStart.Should().Be(1);
         ranges[1].EyEnd.Should().Be(2026);
     }
+
+    [Fact]
+    public void FromDate_on_december_29_2024_is_epidemiological_week_1_of_2025()
+    {
+        var week = EpidemiologicalCalendar.FromDate(new DateOnly(2024, 12, 29));
+
+        week.Year.Should().Be(2025);
+        week.Week.Should().Be(1);
+        week.ToContractString().Should().Be("2025-01");
+    }
+
+    [Fact]
+    public void LastWeeks_skips_the_current_open_week()
+    {
+        var saturdayInWeek37 = new DateTimeOffset(2026, 9, 19, 15, 0, 0, TimeSpan.Zero);
+
+        var weeks = EpidemiologicalCalendar.LastWeeks(saturdayInWeek37);
+
+        weeks.Should().HaveCount(3);
+        weeks[0].ToContractString().Should().Be("2026-36");
+        weeks[1].ToContractString().Should().Be("2026-35");
+        weeks[2].ToContractString().Should().Be("2026-34");
+    }
+
+    [Fact]
+    public void LastWeeks_uses_sao_paulo_civil_date_not_utc()
+    {
+        var utcAlreadySunday = new DateTimeOffset(2026, 9, 20, 2, 0, 0, TimeSpan.Zero);
+
+        var weeks = EpidemiologicalCalendar.LastWeeks(utcAlreadySunday);
+
+        weeks[0].ToContractString().Should().Be("2026-36");
+        weeks[1].ToContractString().Should().Be("2026-35");
+        weeks[2].ToContractString().Should().Be("2026-34");
+    }
 }
